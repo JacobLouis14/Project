831d0f6591377ae287bb11b64424bc9d9f6bbfae
Jacob Louis <louisjacob8@gmail.com>
2023-11-09 17:32:09 +0530

    fix: name

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Landing from "../components/Landing";
-import Discover from "../components/Discover";
+import Discover from "../components/discover";
 import Description from "../components/Description";
 
 export default function Home({ heading }) {
